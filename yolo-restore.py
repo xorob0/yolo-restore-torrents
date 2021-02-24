504774bd7ec602ecf5a3477bc615ebae1c2ad5eb
Tim Simon <tim@mirahi.io>
2021-02-24 09:37:09 +0100

    should fix directory for single file torrents

--- yolo-restore.py
+++ yolo-restore.py
@@ -27,5 +27,6 @@ for torrentFile in torrentsFiles:
             path = path + item
             if file['length'] in source:
                 print('linking ' + source[file['length']] + ' to ' + sys.argv[3] + str(info['name']) + '/' + str(file['path'][0]))
-                os.system('mkdir "' + sys.argv[3] + '/' + str(info['name']) + '/"')
+                if not length in info:
+                    os.system('mkdir "' + sys.argv[3] + '/' + str(info['name']) + '/"')
                 os.system('ln "' + source[file['length']] + '" "' + sys.argv[3] + str(info['name']) + '/' + str(file['path'][0]) + '"')
